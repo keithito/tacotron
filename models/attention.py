44c695bf188e2483c476c9f4e64183c3e92b30e2
Keith Ito <keeeto@gmail.com>
2018-04-01 22:55:50 -0700

    Update attention to work with latest version of TensorFlow.

--- models/attention.py
+++ models/attention.py
@@ -26,33 +26,31 @@ class LocationSensitiveAttention(BahdanauAttention):
       num_units, use_bias=False, dtype=tf.float32, name='location_layer')
 
 
-  def __call__(self, query, previous_alignments):
+  def __call__(self, query, state):
     '''Score the query based on the keys and values.
-
     This replaces the superclass implementation in order to add in the location term.
-
-    TODO: it looks like the signature of this method is going to change in TF 1.5
-
     Args:
       query: Tensor of shape `[N, num_units]`.
-      previous_alignments: Tensor of shape `[N, T_in]`
+      state: Tensor of shape `[N, T_in]`
     Returns:
       alignments: Tensor of shape `[N, T_in]`
+      next_state: Tensor of shape `[N, T_in]`
     '''
     with tf.variable_scope(None, 'location_sensitive_attention', [query]):
-      expanded_alignments = tf.expand_dims(previous_alignments, axis=2)   # [N, T_in, 1]
-      f = self.location_conv(expanded_alignments)                         # [N, T_in, 10]
-      processed_location = self.location_layer(f)                         # [N, T_in, num_units]
+      expanded_alignments = tf.expand_dims(state, axis=2)               # [N, T_in, 1]
+      f = self.location_conv(expanded_alignments)                       # [N, T_in, 10]
+      processed_location = self.location_layer(f)                       # [N, T_in, num_units]
 
       processed_query = self.query_layer(query) if self.query_layer else query  # [N, num_units]
-      processed_query = tf.expand_dims(processed_query, axis=1)           # [N, 1, num_units]
+      processed_query = tf.expand_dims(processed_query, axis=1)         # [N, 1, num_units]
       score = _location_sensitive_score(processed_query, processed_location, self.keys)
-    return self._probability_fn(score, previous_alignments)
+    alignments = self._probability_fn(score, state)
+    next_state = alignments
+    return alignments, next_state
 
 
 def _location_sensitive_score(processed_query, processed_location, keys):
   '''Location-sensitive attention score function.
-
   Based on _bahdanau_score from tensorflow/contrib/seq2seq/python/ops/attention_wrapper.py
   '''
   # Get the number of hidden units from the trailing dimension of keys
