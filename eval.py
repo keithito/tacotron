60d6932f510bf591acb25620290868900b5c0a41
begeekmyfriend <begeekmyfriend@gmail.com>
2019-04-16 10:29:31 +0800

    Loading latest checkpoint on synthesis
    Signed-off-by: begeekmyfriend <begeekmyfriend@gmail.com>

--- eval.py
+++ eval.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import re
+import tensorflow as tf
 from hparams import hparams, hparams_debug_string
 from synthesizer import Synthesizer
 
@@ -19,18 +20,19 @@ sentences = [
 ]
 
 
-def get_output_base_path(checkpoint_path):
+def get_output_base_path(ckpt_path):
   base_dir = os.path.dirname(checkpoint_path)
   m = re.compile(r'.*?\.ckpt\-([0-9]+)').match(checkpoint_path)
   name = 'eval-%d' % int(m.group(1)) if m else 'eval'
   return os.path.join(base_dir, name)
 
 
-def run_eval(args):
+def run_eval(ckpt_dir):
   print(hparams_debug_string())
+  checkpoint = tf.train.get_checkpoint_state(ckpt_dir).model_checkpoint_path
   synth = Synthesizer()
-  synth.load(args.checkpoint)
-  base_path = get_output_base_path(args.checkpoint)
+  synth.load(checkpoint)
+  base_path = get_output_base_path(checkpoint)
   for i, text in enumerate(sentences):
     path = '%s-%03d.wav' % (base_path, i)
     print('Synthesizing: %s' % path)
@@ -40,13 +42,14 @@ def run_eval(args):
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--checkpoint', required=True, help='Path to model checkpoint')
+  parser.add_argument('--checkpoint', default='logs-tacotron', help='Path to model checkpoint')
   parser.add_argument('--hparams', default='',
     help='Hyperparameter overrides as a comma-separated list of name=value pairs')
   args = parser.parse_args()
   os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+  os.environ['CUDA_VISIBLE_DEVICES'] = '1'
   hparams.parse(args.hparams)
-  run_eval(args)
+  run_eval(args.checkpoint)
 
 
 if __name__ == '__main__':
